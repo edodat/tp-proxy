97a9b89620e0e05948f1255cd9df02b34610edc4
Etienne <etienne.dodat@gmail.com>
2013-09-08 02:41:11 +0200

    updated proxy to use a variable as routing table instead of fixed table.

--- app.js
+++ app.js
@@ -18,22 +18,37 @@ var httpProxy = require('http-proxy')
 ///////////////////
 
 var options = {
-    hostnameOnly: true,
-    router: {
-        'api.tp.com': '127.0.0.1:8081',
-        'app.tp.com': '127.0.0.1:8080'
-    },
     https: {
         key: fs.readFileSync('ssl/key.pem', 'utf8'),
         cert: fs.readFileSync('ssl/cert.pem', 'utf8')
     }
 };
 
+var routingTable = {
+    'api.tp.com': {
+        host: 'localhost',
+        port: 8081
+    },
+    'app.tp.com':{
+        host: 'localhost',
+        port: 8080
+    }
+};
+
+var customHandler = function (req, res, proxy){
+    if (req.headers.host) {
+        proxy.proxyRequest(req, res, routingTable[req.headers.host]);
+    } else {
+        res.send(401);
+    }
+
+};
+
 //////////////////
 // START SERVER //
 //////////////////
 
-var proxyServer = httpProxy.createServer(options);
+var proxyServer = httpProxy.createServer(options, customHandler);
 proxyServer.listen(process.env.PORT || 443, function(){
     console.log('Proxy server listening on port ' + proxyServer.address().port);
 });
